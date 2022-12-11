49e65a773cb6744a82e03aef57fa0e95d4183d80
berrujaime <berrujaime@gmail.com>
2022-12-12 00:37:58 +0100

    varias cosas

--- hotelRural/app/src/main/java/es/unizar/eina/hotelRural/HabitationsList.java
+++ hotelRural/app/src/main/java/es/unizar/eina/hotelRural/HabitationsList.java
@@ -1,14 +1,28 @@
 package es.unizar.eina.hotelRural;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HabitationsList extends AppCompatActivity {
+    private Button btn_crear_habitaciones;
 
    @Override
     protected void onCreate(Bundle savedInstanceState){
        super.onCreate(savedInstanceState);
        setContentView(R.layout.listahabitaciones);
+
+       btn_crear_habitaciones = findViewById(R.id.crear_habitacion);
+
+       btn_crear_habitaciones.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(getApplicationContext(), CrearHabitacion.class);
+               startActivity(i);
+           }
+       });
    }
 }
