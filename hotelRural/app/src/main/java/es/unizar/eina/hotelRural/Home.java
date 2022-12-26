b0a84d656d02dd423ed42a04112049c110dc446b
viictorgallardo <vicgallardo88@gmail.com>
2022-12-26 19:04:29 +0100

    Lista habs scrollable

--- hotelRural/app/src/main/java/es/unizar/eina/hotelRural/Home.java
+++ hotelRural/app/src/main/java/es/unizar/eina/hotelRural/Home.java
@@ -37,8 +37,8 @@ public class Home extends AppCompatActivity {
 
         mDbHelper = new HotelDbAdapter(this);
         mDbHelper.open();
-        mDbHelper.createHabitacion(3,"hola", 2,20, 10.1f);
-        //mDbHelper.createHabitacion(5,"hola", 1,40, 10.1f);
+        //mDbHelper.createHabitacion(3,"hola", 2,20, 10.1f);
+        mDbHelper.createHabitacion(33,"hab 33", 2,123, 10.1f);
         //long id = mDbHelper.createHabitacion(7,"primera habitacion", 1,10.0f, 6);
 
         setContentView(R.layout.home);
